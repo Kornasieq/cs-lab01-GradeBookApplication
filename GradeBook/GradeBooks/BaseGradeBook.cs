c10b13acef2af67c39b568763ee0d2d2ace1e321
Kornel Gawronski <kornel.gawronski@microsoft.wsei.edu.pl>
2022-03-31 20:54:01 +0200

    internal to public

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -109,6 +109,25 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            if (IsWeighted)
+            {
+                if (studentType.ToString() =="Honors"||studentType.ToString()=="DualEnrolled")
+                {
+                    switch (letterGrade)
+                    {
+                        case 'A':
+                            return 5;
+                        case 'B':
+                            return 4;
+                        case 'C':
+                            return 3;
+                        case 'D':
+                            return 2;
+                        case 'F':
+                            return 1;
+                    }
+                }
+            }
             switch (letterGrade)
             {
                 case 'A':
